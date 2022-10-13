16384cc83e2babc00fd5a4f642832c17d111f8a5
Jeogn sik <ol37@naver.com>
2022-10-13 22:25:04 +0900

    - 2 commit

--- js/common.js
+++ js/common.js
@@ -1,18 +1,6 @@
-// sly 터치슬라이드 메뉴
-let menu_start = "0";
-$('#mobile_nav').sly({
-  horizontal: 1,
-  itemNav: 'centered',
-  smart: 1,
-  mouseDragging: 1,
-  touchDragging: 1,
-  releaseSwing: 1,
-  startAt: menu_start,
-  speed: 300,
-  elasticBounds: 1,
-  dragHandle: 1,
-  dynamicHandle: 1
-  });
-$(window).resize(function(e) {
-  $('#mobile_nav').sly('reload');
+'use strict';
+// 뒤로가기
+let prev = document.querySelector('.common-ui-btnprev');
+prev.addEventListener('click',()=>{
+  history.back();
 });

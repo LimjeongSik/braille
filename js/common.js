8849eece79f8cd5a3007b922060a4bec8eef5602
LimjeongSik <ol37@naver.com>
2022-10-23 17:19:11 +0900

    fix - 1

--- js/common.js
+++ js/common.js
@@ -1,6 +1,10 @@
-'use strict';
+"use strict";
 // 뒤로가기
-let prev = document.querySelector('.common-ui-btnprev');
-prev.addEventListener('click',()=>{
+let prev = document.querySelector(".common-ui-btnprev");
+prev.addEventListener("click", () => {
   history.back();
+});
+let setting = document.querySelector(".common-ui-btnsetting");
+setting.addEventListener("click", () => {
+  location.href = "setting.html";
 });

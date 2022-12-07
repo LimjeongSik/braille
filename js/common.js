863b9db52317b4649454f2e1eff1dbc48ec67760
LimjeongSik <ol37@naver.com>
2022-12-07 21:08:49 +0900

    ::fix

--- js/common.js
+++ js/common.js
@@ -2,9 +2,20 @@
 // 뒤로가기
 let prev = document.querySelector(".common-ui-btnprev");
 prev.addEventListener("click", () => {
-  history.back();
+    history.back();
 });
 let setting = document.querySelector(".common-ui-btnsetting");
 setting.addEventListener("click", () => {
-  location.href = "setting.html";
+    location.href = "setting.html";
+});
+// level start
+const startPopup = document.querySelector(".starting__popup");
+const start = document.querySelector(".starting__popup .start");
+document.addEventListener("DOMContentLoaded", () => {
+    startPopup.style.opacity = "1";
+    startPopup.style.visibility = "visible";
+});
+start.addEventListener("click", () => {
+    startPopup.style.opacity = "0";
+    startPopup.style.visibility = "hidden";
 });
